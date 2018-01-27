4c23659f389625fb4bfb416c38d45ae5aa723bb7
SadPandaBear <lucas.lentz@hotmail.com>
2018-01-26 22:46:04 -0200

    there we go

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ import Link from 'next/link'
 import Head from '../components/head'
 import SadnessMeter from '../components/SadnessMeter'
 import SearchBar from '../components/SearchBar'
-import { search } from '../lib/api'
 
 export default () => (
   <div className="home">
